bfa51ad0d8669aea08b3b48f207f1c6cfbbfbfb4
javoh1rqaxramonov <ubotirov22@.com>
2024-06-05 13:35:25 +0500

    finished

---
+++ index.js
@@ -0,0 +1,108 @@
+// 1-masala
+// let a = [];
+
+// console.log( a);
+
+// for (let i = 1; i <= 5; i++) {
+//     a.push(i);
+// }
+
+// console.log( a);
+
+//  2-masala
+// let arr = [];
+// let result = arr.join("");
+
+// arr.push("1");
+// arr.push("2");
+// arr.push("3");
+
+// array.pop();
+
+// console.log(result);
+
+// 3-masala
+// let a = []; 
+// let s = a.length; 
+
+// a.unshift("1");
+// a.unshift("2");
+// a.unshift("3");
+// a.unshift("4");
+
+// a.reverse(); 
+
+// console.log(a); 
+// console.log(s); 
+
+// 4-masala
+// let sumArr = [];
+
+// sumArr.unshift("1", "2");
+
+// sumArr.shift();
+
+// let result = sumArr.join('');
+
+// console.log(result);
+
+// 5-masala
+// let arr = [1, 2, 3, 4, 5];
+// let lastElement = arr.pop().toString();
+
+// arr.push(6);
+
+// console.log(arr); 
+// console.log(lastElement); 
+
+// 6-masala
+// let arr = [1, 2, 3, 4, 5];
+
+// arr.push(6);
+
+// let removedElement = arr.pop();
+
+// let stringElement = removedElement.toString();
+
+// console.log(arr);
+// console.log(stringElement);
+
+
+// 8-masala
+// let nums = [2, 4, 5];
+
+// let a = nums.shift();
+// let s = nums.length;
+
+// let d = nums.join('');
+
+// console.log(a);
+// console.log(s);
+
+// 9-masala
+// let sumArr = ["bir", "ikki", "uch"];
+
+// sumArr.push("to'rt", "besh", "olti");
+
+// let uzunlik = sumArr.length;
+
+// sumArr.reverse();
+
+// let stringMassiv = sumArr.join("");
+
+// console.log(stringMassiv); 
+
+// 10-masala
+// let sumArr = ["bir", "ikki", "uch", "tort", "besh"];
+
+// sumArr.pop(); 
+// sumArr.shift(); 
+
+
+// let uzunlik = sumArr.length;
+
+
+// let teskariMassiv = sumArr.reverse();
+
+// console.log(uzunlik); 
+// console.log(teskariMassiv); 
